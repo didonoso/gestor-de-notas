ddaf640e6a849e97a51c5b865f6b8eaa753a46cd
devdiegodv <donosoveradiego@gmail.com>
2025-09-11 17:42:40 -0300

    mejoras de seguridad y documentación

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -19,7 +19,19 @@ indexController.renderIndex = (req, res) => {
  * @param {Object} req - El objeto de la solicitud (request).
  * @param {Object} res - El objeto de la respuesta (response).
  */
+/**
+ * Renderiza la página de Preguntas Frecuentes (FAQ) con mejora de seguridad.
+ * Verifica la cabecera Referer para mitigar accesos sospechosos (protección básica contra CSRF).
+ *
+ * @param {Object} req - El objeto de la solicitud (request).
+ * @param {Object} res - El objeto de la respuesta (response).
+ */
 indexController.renderFaq = (req, res) => {
+    const referer = req.get('Referer');
+    // Permite solo si la petición viene de la misma aplicación o no tiene referer
+    if (referer && !referer.startsWith(req.protocol + '://' + req.get('host'))) {
+        return res.status(403).send('Acceso no autorizado.');
+    }
     res.render('faq');
 };
 

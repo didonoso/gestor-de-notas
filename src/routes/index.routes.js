f062adc61ba483d90d4e6df5f29f609ace6ab1c2
devdiegodv <donosoveradiego@gmail.com>
2025-09-11 17:43:11 -0300

    se añade middleware para validar si usuario está autenticado en rutas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -3,40 +3,21 @@ const router = Router();
 
 // Importamos las funciones del controlador
 const { renderIndex, renderFaq } = require('../controllers/index.controller');
+const { isAuthenticated } = require('../helpers/auth');
 
 /**
  * Ruta principal del sitio web
  * @route GET /
  * @desc Renderiza la página principal del gestor de productos
  */
-router.get('/', async (req, res, next) => {
-    try {
-        renderIndex(req, res);
-    } catch (error) {
-        console.error('Error al cargar la ruta de inicio: ', error);
-        res.status(500).json({
-            message: 'Ocurrió un error al cargar la ruta de inicio.',
-            error: error.message || error
-        });
-    }
-});
+router.get('/', renderIndex);
 
 /**
  * Ruta de Preguntas Frecuentes (FAQ)
  * @route GET /faq
  * @desc Renderiza la página de preguntas frecuentes
  */
-router.get('/faq', (req, res, next) => {
-    try {
-        renderFaq(req, res);
-    } catch (error) {
-        console.error('Error al cargar la ruta de FAQ: ', error);
-        res.status(500).json({
-            message: 'Ocurrió un error al cargar la ruta de FAQ.',
-            error: error.message || error
-        });
-    }
-});
+router.get('/faq', isAuthenticated, renderFaq);
 
 // Exportamos el router para que pueda ser usado en otros archivos
 module.exports = router;
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -2,28 +2,36 @@ const { Router } = require('express');
 const router = Router();
 
 // Importamos las funciones del controlador de notas
-const { renderNoteForm, createNewNote, renderNotes, renderEditForm, updateNote, deleteNote } = require('../controllers/notes.controller');
+const { 
+    renderNoteForm, 
+    createNewNote, 
+    renderNotes, 
+    renderEditForm, 
+    updateNote, 
+    deleteNote 
+} = require('../controllers/notes.controller');
+const { isAuthenticated } = require('../helpers/auth');
 
 /**
  * Ruta para renderizar el formulario de agregar una nueva nota
  * @route GET /notas/agregar
  * @desc Renderiza un formulario para agregar una nueva nota
  */
-router.get('/notas/agregar', renderNoteForm);
+router.get('/notas/agregar', isAuthenticated, renderNoteForm);
 
 /**
  * Ruta para crear una nota nueva
  * @route POST /notas/nota-nueva
  * @desc Procesa y crea una nota nueva con los datos enviados
  */
-router.post('/notas/nota-nueva', createNewNote);
+router.post('/notas/nota-nueva', isAuthenticated, createNewNote);
 
 /**
  * Ruta para listar todas las notas
  * @route GET /notas
  * @desc Renderiza una lista de todas las notas existentes
  */
-router.get('/notas', renderNotes);
+router.get('/notas', isAuthenticated, renderNotes);
 
 /**
  * Ruta para renderizar el formulario de edición de una nota existente
@@ -31,7 +39,7 @@ router.get('/notas', renderNotes);
  * @param {string} id - ID de la nota a editar
  * @desc Renderiza el formulario de edición de la nota con el ID especificado
  */
-router.get('/notas/editar/:id', renderEditForm);
+router.get('/notas/editar/:id', isAuthenticated, renderEditForm);
 
 /**
  * Ruta para actualizar una nota existente
@@ -39,7 +47,7 @@ router.get('/notas/editar/:id', renderEditForm);
  * @param {string} id - ID de la nota a actualizar
  * @desc Actualiza la nota con el ID especificado
  */
-router.put('/notas/editar/:id', updateNote);
+router.put('/notas/editar/:id', isAuthenticated, updateNote);
 
 /**
  * Ruta para eliminar una nota existente
@@ -47,7 +55,7 @@ router.put('/notas/editar/:id', updateNote);
  * @param {string} id - ID de la nota a eliminar
  * @desc Elimina la nota con el ID especificado
  */
-router.delete('/notas/borrar/:id', deleteNote);
+router.delete('/notas/borrar/:id', isAuthenticated, deleteNote);
 
 // Exportamos el router para que pueda ser utilizado en otros archivos
 module.exports = router;
